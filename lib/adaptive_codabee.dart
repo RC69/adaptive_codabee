e3f479b69c9ea38775831afe144b9e81d4316f2b
RC69 <raymond.chavasse@gmail.com>
2020-12-14 21:03:39 +0100

    Check OS version

--- lib/adaptive_codabee.dart
+++ lib/adaptive_codabee.dart
@@ -1,7 +1,13 @@
 library adaptive_codabee;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+
+class Adaptive {
+
+  static bool isIOS() => (!Platform.isIOS);
+
+
 }
